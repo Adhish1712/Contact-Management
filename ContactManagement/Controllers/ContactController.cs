ce33021f945a4f2a5f324474590bbf470ce0f1af
Adhish1712 <43907805+Adhish1712@users.noreply.github.com>
2019-12-10 19:56:39 +0530

    ContactController.cs
    Added excepttion

--- ContactManagement/Controllers/ContactController.cs
+++ ContactManagement/Controllers/ContactController.cs
@@ -1,11 +1,14 @@
-﻿using DomainModel.Contact;
+using DomainModel.Contact;
 using IBLL.IService;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 
 namespace ContactManagement.Controllers
 {
+    [RoutePrefix("api/Contact")]
     public class ContactController : ApiController
     {
         private readonly IContactService _contactService;
@@ -15,9 +18,9 @@ namespace ContactManagement.Controllers
             _contactService = contactService;
         }
 
-        // GET: api/Contact
+        // GET: api/Contacts
         [HttpGet]
-        [Route("api/Contact/Contacts")]
+        [Route("Contacts")]
         public IHttpActionResult Contacts()
         {
             return Ok<IEnumerable<Contact>>(_contactService.GetContacts().ToList());
@@ -25,15 +28,27 @@ namespace ContactManagement.Controllers
 
         // GET: api/GetContact/5
         [HttpGet]
-        [Route("api/Contact/Contact/{id}")]
+        [Route("Contact/{id}")]
         public Contact Contact(int id)
         {
-            return _contactService.GetContact(id);
+            Contact data = _contactService.GetContact(id);
+
+            if (data == null)
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.NotFound)
+                {
+                     Content = new StringContent(string.Format("No Contact found with ID = {0}", id)),  
+                     ReasonPhrase = "Contact Not Found"
+                };
+
+                throw new HttpResponseException(response);
+            }
+            return data;
         }
 
         // POST: api/CreateContact
         [HttpPost]
-        [Route("api/Contact/AddContact")]
+        [Route("AddContact")]
         public Contact Contact([FromBody]Contact contact)
         {
             return _contactService.CreateContact(contact);
@@ -41,7 +56,7 @@ namespace ContactManagement.Controllers
 
         // PUT: api/ChangeContactStatus/5
         [HttpPut]
-        [Route("api/Contact/UpdateContact")]
+        [Route("UpdateContact")]
         public bool UpdateContactStatus([FromBody]Contact contact)
         {
             return _contactService.ChangeContactStatus(contact);
